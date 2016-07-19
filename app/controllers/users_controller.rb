8fc1784509e087283ab3223715fba211888e2dff
Jack Lieblich <jacklieb220@gmail.com>
2016-07-19 21:17:37 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to BuzzList!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
 
   root 'static_pages#home'
-
-  get 'static_pages/home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   
 end
