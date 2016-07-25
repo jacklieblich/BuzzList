c10d45f7624e70c83ea01b8b18b397c8ecd2c0bc
Jack Lieblich <jacklieb220@gmail.com>
2016-07-25 19:22:12 +0000

    Add shows

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719041019) do
+ActiveRecord::Schema.define(version: 20160725022437) do
+
+  create_table "shows", force: :cascade do |t|
+    t.string   "title"
+    t.string   "poster"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +27,7 @@ ActiveRecord::Schema.define(version: 20160719041019) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
