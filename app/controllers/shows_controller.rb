bf9461a2a634860e6a0096646bc37c9089a9135a
Jack Lieblich <jacklieb220@gmail.com>
2016-08-17 00:41:27 +0000

    fix ordering of episodes

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -11,30 +11,31 @@ class ShowsController < ApplicationController
   def show
       @show = Show.find(params[:id])
       @seasons= @show.seasons
-      
+      best = false
       
       if params[:season_id] == nil
         params[:season_id] = @seasons
+        best = true
       end
       
       episodes = Episode.where(season_id: params[:season_id])
       
       if params[:items] == 'Quote'
         @items = Quote.where(episode_id: episodes)
-        if params[:season_id].class != String
+        if best
           @items = @items.sort_by { |quote| quote.likes.count }
             
         end
         else
           if params[:items] == 'Clip'
             @items = Clip.where(episode_id: episodes)
-            if params[:season_id].class != String
+            if best
               @items = @items = @items.sort_by { |clip| clip.likes.count }
             end
             else
               params[:items] = 'Episode'
               @items = episodes
-              if params[:season_id].class != String
+              if best
                 @items = @items.order('episodes.buzzlisted DESC').first(10)
               end
           end
