6730a757ebcec36833cfe5fcadea9b166c3cb576
Jack Lieblich <jacklieb220@gmail.com>
2016-08-22 13:47:36 -0400

    add callback to update listepisode to prevent displaying favorite show more than once in feed

--- app/models/list_episode.rb
+++ app/models/list_episode.rb
@@ -5,11 +5,24 @@ class ListEpisode < ActiveRecord::Base
   belongs_to :episode
   has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
   before_destroy :decrement_buzzlisted
+  after_update :destroy_activity
   
   protected
   
   def decrement_buzzlisted
     episode.decrement(:buzzlisted)
     episode.save
+  end
+
+  def destroy_activity
+    if ranking == 1
+      activity_id = PublicActivity::Activity.find_by(trackable_id: id, trackable_type: "ListEpisode", key: "list_episode.update").id
+      list_update_activities = PublicActivity::Activity.where(key:"list_episode.update")
+      list_update_activities.each do |activity|
+        if activity.trackable.list_id == list_id && activity.id != activity_id
+          activity.destroy
+        end
+      end
+    end
   end
 end
