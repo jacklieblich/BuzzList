26bc7c26ef1ba1f83a2f20a14629a39e54d005f5
Jack Lieblich <jacklieb220@gmail.com>
2016-08-23 23:31:48 -0400

    add notifications v1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @feed_items = PublicActivity::Activity.all.paginate(page: params[:page], per_page: 10).order("created_at desc")
       @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:page], per_page: 10).order("created_at desc")
+      @notifications = PublicActivity::Activity.where(trackable_type: "Like").order("created_at desc").reject {|notification| notification.trackable.likable.user != current_user || notification.owner == current_user}.paginate(page: params[:page], per_page: 5)
+
     end
     if params[:search]
       @shows = Show.search(params[:search])
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
+
    # Follows a user.
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
