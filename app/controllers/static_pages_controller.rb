9fac083df27adb0a6151c737ba80e130f8a0fa22
Jack Lieblich <jacklieb220@gmail.com>
2016-08-30 18:30:31 -0400

    fix home page pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,12 +3,12 @@ class StaticPagesController < ApplicationController
   
   def home
     if logged_in?
-      @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).paginate(page: params[:page], per_page: 10).order("created_at desc")
-      @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:page], per_page: 10).order("created_at desc")
-      @notifications = (current_user.quote_likes + current_user.clip_likes).paginate(page: params[:page], per_page: 4)
-      @tagged_in = current_user.passive_tags.paginate(page: params[:page], per_page: 4)
+      @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).paginate(page: params[:all_feed], per_page: 10).order("created_at desc")
+      @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:my_feed], per_page: 10).order("created_at desc")
+      @likes = (current_user.quote_likes + current_user.clip_likes).paginate(page: params[:likes], per_page: 4)
+      @tagged_in = current_user.passive_tags.paginate(page: params[:shared], per_page: 4)
     else
-      @feed_items = PublicActivity::Activity.all.paginate(page: params[:page], per_page: 10).order("created_at desc")
+      @feed_items = PublicActivity::Activity.all.paginate(page: params[:all_feed], per_page: 10).order("created_at desc")
     end
     if params[:search]
       @shows = Show.search(params[:search])
