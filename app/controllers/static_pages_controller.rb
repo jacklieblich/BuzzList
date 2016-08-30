814930e1c4184dd686f14c9c08d0a21b4c2f0cf0
Jack Lieblich <jacklieb220@gmail.com>
2016-08-30 17:12:56 -0400

    fix feed items when not logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,13 @@ class StaticPagesController < ApplicationController
   require 'will_paginate/array'
   
   def home
+    if logged_in?
       @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).paginate(page: params[:page], per_page: 10).order("created_at desc")
-      if logged_in?
       @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:page], per_page: 10).order("created_at desc")
       @notifications = (current_user.quote_likes + current_user.clip_likes).paginate(page: params[:page], per_page: 4)
       @tagged_in = current_user.passive_tags.paginate(page: params[:page], per_page: 4)
+    else
+      @feed_items = PublicActivity::Activity.all.paginate(page: params[:page], per_page: 10).order("created_at desc")
     end
     if params[:search]
       @shows = Show.search(params[:search])
