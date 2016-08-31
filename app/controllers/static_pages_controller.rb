dc7437939e75bc4fdd60960bcf83e9ba4d0a61b0
Jack Lieblich <jacklieb220@gmail.com>
2016-08-31 19:38:20 -0400

    order notifications and add timestamps to everything

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).paginate(page: params[:all_feed], per_page: 15).order("created_at desc")
       @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:my_feed], per_page: 15).order("created_at desc")
-      @likes = (current_user.quote_likes.where.not(user_id: current_user) + current_user.clip_likes.where.not(user_id: current_user)).paginate(page: params[:likes], per_page: 4)
-      @tagged_in = current_user.passive_tags.paginate(page: params[:shared], per_page: 4)
+      @likes = (current_user.quote_likes.where.not(user_id: current_user) + current_user.clip_likes.where.not(user_id: current_user)).sort_by{|like| like.created_at}.reverse.paginate(page: params[:likes], per_page: 4)
+      @tagged_in = current_user.passive_tags.order("created_at desc").paginate(page: params[:shared], per_page: 4)
     else
       @feed_items = PublicActivity::Activity.all.paginate(page: params[:all_feed], per_page: 20).order("created_at desc")
     end
