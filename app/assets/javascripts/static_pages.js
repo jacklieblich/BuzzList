dcc1b7100c09275192f273077835499bb0fb7033
Jack Lieblich <jacklieb220@gmail.com>
2016-08-31 18:46:20 -0400

    close notification tabs on second click

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -11,9 +11,19 @@ $(document).on("turbolinks:load", function() {
 			isFitWidth: true
 		});
 	});
+	$('#notification-pills a').click(function (e) {
+    var tab = $(this);
+    if(tab.parent('li').hasClass('active')){
+        window.setTimeout(function(){
+            $("#notification-content .tab-pane").removeClass('active');
+            tab.parent('li').removeClass('active');
+        },1);
+    }
+});
 });
 $(document).on("click", ".pagination a", function(){
 	$(this).parent().parent().html("Loading Sick Shit . . .");
 	$.getScript(this.href);
 	return false;
 });
+
