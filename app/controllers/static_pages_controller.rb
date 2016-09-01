153191c8120b697141167b0ef2a2c9411d151f5c
Jack Lieblich <jacklieb220@gmail.com>
2016-09-01 19:31:28 -0400

    remove likes from everything feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,12 +3,13 @@ class StaticPagesController < ApplicationController
   
   def home
     if logged_in?
-      @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).paginate(page: params[:all_feed], per_page: 15).order("created_at desc")
+
+      @feed_items = PublicActivity::Activity.where.not(owner_id: current_user).where.not(trackable_type: "Like").order("created_at desc").paginate(page: params[:all_feed], per_page: 15)
       @friend_feed_items = PublicActivity::Activity.where(owner_id: current_user.following_ids, owner_type:"User").paginate(page: params[:my_feed], per_page: 15).order("created_at desc")
       @likes = (current_user.quote_likes.where.not(user_id: current_user) + current_user.clip_likes.where.not(user_id: current_user)).sort_by{|like| like.created_at}.reverse.paginate(page: params[:likes], per_page: 4)
       @tagged_in = current_user.passive_tags.order("created_at desc").paginate(page: params[:shared], per_page: 4)
     else
-      @feed_items = PublicActivity::Activity.all.paginate(page: params[:all_feed], per_page: 20).order("created_at desc")
+      @feed_items = PublicActivity::Activity.where.not(trackable_type: "Like").paginate(page: params[:all_feed], per_page: 20).order("created_at desc")
     end
     if params[:search]
       @shows = Show.search(params[:search])
