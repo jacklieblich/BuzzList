211e2434f8036f9be15bb42463cf98933cc7b03c
Jack Lieblich <jacklieb220@gmail.com>
2016-09-01 12:45:13 -0400

    change youtube vid thumbnail so exists for all vids

--- app/helpers/clips_helper.rb
+++ app/helpers/clips_helper.rb
@@ -2,7 +2,7 @@ module ClipsHelper
 
   def thumbnail(video_url)
   	video_id = video_url.split("=").last
-  	image_tag("//img.youtube.com/vi/"+video_id+"/maxresdefault.jpg", class: "youtube-thumbnail", id: video_id)
+  	image_tag("//img.youtube.com/vi/"+video_id+"/hqdefault.jpg", class: "youtube-thumbnail", id: video_id)
   end
 
 end
