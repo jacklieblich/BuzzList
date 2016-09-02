4effff6b3aa558ffbe210fb37d027ba8a9423cc0
Jack Lieblich <jacklieb220@gmail.com>
2016-09-01 21:16:42 -0400

    only show clips and quotes created on everything feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  
-
-  #get 'sessions/new'
-
-  get '/signup', to: 'users#new'
 
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
